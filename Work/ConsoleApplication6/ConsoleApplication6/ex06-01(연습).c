6072f3430ca4b9dce011918c2cb26e6dc10e56f4
Han99 <senssos@daum.net>
2018-01-13 09:59:04 +0900

    Week2

---
+++ Work/ConsoleApplication6/ConsoleApplication6/ex06-01(연습).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void)
+{
+	int number1 = 1;
+	int number2 = 1;
+
+	int answer1 = 1;
+	int answer2 = 1;
+	int answer3 = 1;
+	int answer4 = 1;
+	int answer5 = 1;
+
+	printf("2개의 정수를 입력하세요.\n");
+	printf("input : ");
+	scanf("%d %d", &number1, &number2);
+
+	answer1 = number1 + number2;
+	answer2 = number1 - number2;
+	answer3 = number1 * number2;
+	answer4 = number1 / number2;
+	answer5 + number1 % number2;
+
+	printf("더하기 : %d\n", answer1);
+	printf("빼기   : %d\n", answer2);
+	printf("곱하기 : %d\n", answer3);
+	printf("나누기 : %d\n", answer4);
+	printf("나머지 : %d\n", answer5);
+
+	system("pause");
+}
+
--- Work/ConsoleApplication6/ConsoleApplication6/ex06-01.c
+++ Work/ConsoleApplication6/ConsoleApplication6/ex06-01.c
@@ -3,28 +3,27 @@
 
 void main(void)
 {
-	int number1 = 1;
-	int number2 = 1;
+	int num1 = 10, num2 = 5;
 
-	int answer1 = 1;
-	int answer2 = 1;
-	int answer3 = 1;
-	int answer4 = 1;
-
-	printf("2개의 정수를 입력하세요.\n");
 	printf("input : ");
-	scanf("%d %d", &number1, &number2);
+	// 첫번째 %d 값은 num1의 주소값에 저장하시오.
+	// 두번째 %d 값은 num2의 주소값에 저장하시오.
+	scanf("%d %d", &num1, &num2);
 
-	answer1 = number1 + number2;
-	answer2 = number1 - number2;
-	answer3 = number1 * number2;
-	answer4 = number1 / number2;
+	printf("입력값 : %d %d \n", num1, num2);
 
-	printf("더하기 : %d\n", answer1);
-	printf("빼기   : %d\n", answer2);
-	printf("곱하기 : %d\n", answer3);
-	printf("나누기 : %d\n", answer4);
+	int a = num1 + num2;
+	int m = num1 - num2;
+	int c = num1 * num2; // 곱하기 : 55
+	int d = num1 / num2; // 나누기 : 2
+	int r = num1 % num2; // 나머지 : 1, 11-5*(2(몫))=1
+
+	printf("+: %d \n", a);
+	printf("-: %d \n", m);
+	printf("*: %d \n", c);
+	printf("/: %d \n", d);
+	printf("%%: %d \n", r);
+
 
 	system("pause");
 }
-
