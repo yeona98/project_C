cbadd5f6f9f63ebc109f9c6896687c03b8501c55
Han99 <senssos@daum.net>
2018-01-20 12:34:41 +0900

    "for" finished

--- Work/ConsoleApplication8/ConsoleApplication8/ex08-01.c
+++ Work/ConsoleApplication8/ConsoleApplication8/ex08-01.c
@@ -11,5 +11,15 @@ void main(void)
 		printf("%d", i);
 	}
 
+	// for문을 이용한 합계 구하기
+	int sum = 0;
+	for (int i = 0;
+		     i <= 10;
+		     i = i + 1)
+	{
+		sum = sum + i;
+	}
+	printf("1부터 10까지의 합계는 %d", sum);
+
 	system("pause");
 }
